44ebc201376ea78677b21690cc207300a22c0880
slybootslion <slybootslion@vip.qq.com>
2019-12-04 17:54:40 +0800

    瀑布流练习，做不出

---
+++ LinUI瀑布流练习/pages/home/home.js
@@ -0,0 +1,39 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.getData()
+  },
+  getData() {
+    wx.request({
+      url: 'http://se.7yue.pro/v1/spu/latest',
+      method: 'GET',
+      header: {
+        appkey: 'YYnRnGHl86NFBoJb'
+      },
+      success: (res) => {
+        this.setData({
+          list: res.data.items
+        })
+        this.setWaterflowData()
+      }
+    })
+  },
+  setWaterflowData() {
+    const {list} = this.data
+    console.log(list)
+    wx.lin.renderWaterFlow(list, false, () => {
+      console.log('渲染成功')
+    })
+  }
+})
